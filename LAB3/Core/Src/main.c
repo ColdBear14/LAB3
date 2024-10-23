6abe6bb67b0d55ac7030951de0711c8e9cf34d6c
LAPTOP-L4QLQTHA\Hy <ngxuanhy1405@gmail.com>
2024-10-23 18:30:47 +0700

    init lab3

--- LAB3/Core/Src/main.c
+++ LAB3/Core/Src/main.c
@@ -201,8 +201,8 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, PA0_Pin|PA1_Pin|PA2_Pin|PA3_Pin
-                          |PA4_Pin|PA5_Pin|PA6_Pin|PA7_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, PA1_Pin|PA2_Pin|PA3_Pin|PA4_Pin
+                          |PA5_Pin|PA6_Pin|PA7_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : BUTTON1_Pin */
   GPIO_InitStruct.Pin = BUTTON1_Pin;
@@ -210,10 +210,16 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(BUTTON1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA0_Pin PA1_Pin PA2_Pin PA3_Pin
-                           PA4_Pin PA5_Pin PA6_Pin PA7_Pin */
-  GPIO_InitStruct.Pin = PA0_Pin|PA1_Pin|PA2_Pin|PA3_Pin
-                          |PA4_Pin|PA5_Pin|PA6_Pin|PA7_Pin;
+  /*Configure GPIO pin : PA0_Pin */
+  GPIO_InitStruct.Pin = PA0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(PA0_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA1_Pin PA2_Pin PA3_Pin PA4_Pin
+                           PA5_Pin PA6_Pin PA7_Pin */
+  GPIO_InitStruct.Pin = PA1_Pin|PA2_Pin|PA3_Pin|PA4_Pin
+                          |PA5_Pin|PA6_Pin|PA7_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
